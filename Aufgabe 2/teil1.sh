664a5201542c0d77272e0c2109bc061bea296d2c
Luca-1802 <luca.theilen@gmx.net>
2024-04-24 14:32:45 +0200

    changed output formatting for teil1.sh

--- Aufgabe 2/teil1.sh
+++ Aufgabe 2/teil1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Eine Funktion für farbigen Output
+colored_header() {
+	printf "\e[1;34m$1\e[1;0m"
+}
+
 # Alle übergebenen Kommandozeilenargumente
 args=0
 all_args=""
@@ -13,28 +18,28 @@ done
 all_args="${all_args%, }"
 
 # Ausgeben der Argumente
-echo "Übergebene Argumente[$args]: $all_args"
+printf "$(colored_header "Übergebene Argumente[$args]:")\n$all_args\n\n"
 
 # Versionsnummer des Linux-Kernels
 kernel_version=$(uname -r)
-echo "Linux-Kernel-Version: $kernel_version"
+printf "$(colored_header "Linux-Kernel-Version:")\n$kernel_version\n\n"
 
 # Anzahl der CPU-Kerne
 cpu_cores=$(cat /proc/cpuinfo | grep -c '^cpu cores' | uniq)
-echo "Anzahl der CPU-Kerne: $cpu_cores"
+printf "$(colored_header "Anzahl der CPU-Kerne:")\n$cpu_cores\n\n"
 
 # Menge des freien Hauptspeichers
 free_memory=$(grep -oP 'MemFree:\s+\K\d+' /proc/meminfo)
-echo "Freier Hauptspeicher: $free_memory kB"
+printf "$(colored_header "Freier Hauptspeicher:")\n$free_memory kB\n\n"
 
 # IP-Adresse des Rechners
-ip_address=$(hostname -I)
-echo "IP-Adresse des Rechners: $ip_address"
+ip_address=$(hostname -i)
+printf "$(colored_header "IP-Adresse des Rechners:")\n$ip_address\n\n"
 
 # Liste der angemeldeten Nutzer
-logged_in_users=$(who | awk '{print $1}')
-echo "Angemeldete Nutzer: $logged_in_users"
+logged_in_users=$(who)
+printf "$(colored_header "Angemeldete Nutzer:")\n$logged_in_users\n\n"
 
 # Belegter Speicherplatz im aktuellen Verzeichnis
 used_space=$(du -sh . | awk '{print $1}')
-echo "Belegter Speicherplatz im aktuellen Verzeichnis: $used_space"
+printf "$(colored_header "Belegter Speicherplatz im aktuellen Verzeichnis:")\n$used_space\n\n"
